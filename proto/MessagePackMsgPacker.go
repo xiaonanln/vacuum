d8fd2d9e5812009a4f46be16de8c775f9fdd294a
xiaonanln <xiaonanln@gmail.com>
2016-10-15 20:11:41 +0800

    proto

---
+++ proto/MessagePackMsgPacker.go
@@ -0,0 +1,27 @@
+package proto
+
+import (
+	"bytes"
+
+	"gopkg.in/vmihailenco/msgpack.v2"
+)
+
+type MessagePackMsgPacker struct{}
+
+func (mp *MessagePackMsgPacker) PackMsg(msg interface{}, buf []byte) ([]byte, error) {
+	//return msgpack.Marshal(msg)
+	buffer := bytes.NewBuffer(buf)
+
+	encoder := msgpack.NewEncoder(buffer)
+	err := encoder.Encode(msg)
+	if err != nil {
+		return buf, err
+	}
+	buf = buffer.Bytes()
+	return buf, nil
+}
+
+func (mp *MessagePackMsgPacker) UnpackMsg(data []byte, msg interface{}) error {
+	err := msgpack.Unmarshal(data, msg)
+	return err
+}
--- proto/proto_test.go
+++ proto/proto_test.go
@@ -1,24 +1,53 @@
 package proto
 
 import (
-	"log"
 	"testing"
+
+	"github.com/xiaonanln/vacuum/uuid"
 )
 
-func TestJSONMsgPacker(t *testing.T) {
-	var packer MsgPacker
-	packer = &JSONMsgPacker{}
-	msg := RegisterVacuumServerMsg{
-		ID: "abc",
-	}
-	buf := make([]byte, 0, 100)
-	buf, err := packer.PackMsg(msg, buf)
+type testMsg struct {
+	ID        string
+	F1        float64
+	F2        int
+	ListField []interface{}
+	MapField  map[string]interface{}
+}
 
-	log.Printf("MsgPack: %v => %s, error %v", msg, string(buf), err)
-	var restoreMsg RegisterVacuumServerMsg
-	err = packer.UnpackMsg(buf, &restoreMsg)
-	log.Printf("MsgPack: %s => %v, error %v", string(buf), msg, err)
-	if msg.ID != restoreMsg.ID {
-		t.Fail()
+func BenchmarkJSONMsgPacker(b *testing.B) {
+	benchmarkMsgPacker(b, &JSONMsgPacker{})
+}
+
+func BenchmarkMessagePackMsgPacker(b *testing.B) {
+	benchmarkMsgPacker(b, &MessagePackMsgPacker{})
+}
+
+func BenchmarkGobMsgPacker(b *testing.B) {
+	benchmarkMsgPacker(b, &GobMsgPacker{})
+}
+
+func benchmarkMsgPacker(b *testing.B, packer MsgPacker) {
+	msg := testMsg{
+		ID:        "abc",
+		F1:        0.123124234,
+		ListField: []interface{}{1, 2, 3, "abc", "def"},
+		MapField:  map[string]interface{}{},
+	}
+	for i := 0; i < 100; i++ {
+		msg.MapField[uuid.GenUUID()] = uuid.GenUUID()
+	}
+
+	for i := 0; i < b.N; i++ {
+
+		buf := make([]byte, 0, 100)
+		buf, _ = packer.PackMsg(msg, buf)
+
+		//log.Printf("MsgPack: %v => %s(%d), error %v", msg, string(buf), len(buf), err)
+		var restoreMsg RegisterVacuumServerMsg
+		_ = packer.UnpackMsg(buf, &restoreMsg)
+		//log.Printf("MsgPack: %s => %v, error %v", string(buf), msg, err)
+		if msg.ID != restoreMsg.ID {
+			b.Fail()
+		}
 	}
 }
